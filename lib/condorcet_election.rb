f1a17778629a35f25f788bc6dc99c67f433882b1
Philippe Guégan <philippe.guegan@gmail.com>
2017-05-16 19:35:37 +0200

    Petite refactoring

--- lib/condorcet_election.rb
+++ lib/condorcet_election.rb
@@ -12,7 +12,7 @@ module VictoriousCandidate
       @victories ||= 0
     end
 
-    def fight_with(other)
+    def compare(other)
       if votes > other.votes
         self.victories += 1
       else
@@ -40,11 +40,15 @@ class CondorcetElection < Election
   private
 
   def compare(c_1, c_2)
+    # Avec la méthode de Condorcet, on compare les candidats
+    # deux à deux dans chaque classement
+    # On réinitialise le nombre de votes de chaque candidat
+    # à chaque confrontation (chaque duel est indépendant)
     c_1.votes, c_2.votes = 0, 0
     @rankings.each do |ranking|
       ranking.compare c_1, c_2
     end
-    c_1.fight_with c_2
+    c_1.compare c_2
   end
 
 end
